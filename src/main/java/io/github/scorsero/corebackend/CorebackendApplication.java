4186c62578fc5bfa4c1a5cf7ee264f92871b1738
dmi3coder <dmi3coder@gmail.com>
2017-08-17 11:21:59 +0300

    Scorsero core backend arrival 🍾

---
+++ src/main/java/io/github/scorsero/corebackend/CorebackendApplication.java
@@ -0,0 +1,12 @@
+package io.github.scorsero.corebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorebackendApplication.class, args);
+	}
+}
