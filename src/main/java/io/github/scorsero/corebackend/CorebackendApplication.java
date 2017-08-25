3ccd7468daa582de51114d5957a6b84ef1d0d607
dmi3coder <dmi3coder@gmail.com>
2017-08-25 21:32:28 +0300

    Ignoring db.properties not found exception

--- src/main/java/io/github/scorsero/corebackend/CorebackendApplication.java
+++ src/main/java/io/github/scorsero/corebackend/CorebackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("db.properties")
+@PropertySource(value = "db.properties",ignoreResourceNotFound = true)
 public class CorebackendApplication {
 
 	public static void main(String[] args) {
