d423338d0a6a1a215b050ddfe4053124b2a9338d
dmi3coder <dmi3coder@gmail.com>
2017-09-16 22:26:52 +0300

    Improved user authentication Enabled debug logging over app logs

--- src/main/java/io/github/scorsero/corebackend/tcp/CommunicationThread.java
+++ src/main/java/io/github/scorsero/corebackend/tcp/CommunicationThread.java
@@ -3,16 +3,18 @@ package io.github.scorsero.corebackend.tcp;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketException;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("prototype")
 public class CommunicationThread extends Thread {
+
+  @Value("${socket.timeout:2000}")
+  private Integer timeout;
 
   private Logger logger = LoggerFactory.getLogger(CommunicationThread.class);
 
@@ -27,17 +29,20 @@ public class CommunicationThread extends Thread {
     logger.debug("Socket connected: {}", socket.isConnected());
     byte[] buf;
     try {
-      socket.setSoTimeout(2000);
+      socket.setKeepAlive(true);
+      socket.setSoTimeout(timeout);
     } catch (SocketException e) {
       e.printStackTrace();
     }
-    while (socket.isConnected()) {
+    boolean shutdown = false;
+    while (socket.isConnected() && !shutdown) {
       try {
-        buf = new byte[1024];
-        while (socket.getInputStream().read() != -1){
-
+        buf = new byte[20];
+        while (socket.getInputStream().read(buf) != -1) {
+          // TODO: 9/16/17 connect protobuf
         }
       } catch (IOException e) {
+        shutdown = true;
         e.printStackTrace();
       }
     }
