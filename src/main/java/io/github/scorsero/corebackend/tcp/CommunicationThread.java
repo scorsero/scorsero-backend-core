d412a42f08b179bca4a5479710f6fc09fa3bef06
dmi3coder <dmi3coder@gmail.com>
2017-09-19 13:43:04 +0300

    Checking login request params

--- src/main/java/io/github/scorsero/corebackend/tcp/CommunicationThread.java
+++ src/main/java/io/github/scorsero/corebackend/tcp/CommunicationThread.java
@@ -1,6 +1,9 @@
 package io.github.scorsero.corebackend.tcp;
 
 
+import io.github.scorsero.corebackend.data.User;
+import io.github.scorsero.corebackend.data.repository.UserRepository;
+import io.github.scorsero.transport.Transport.LoginRequest;
 import io.github.scorsero.transport.Transport.TransportEnvelope;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -8,6 +11,7 @@ import java.net.Socket;
 import java.net.SocketException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -15,6 +19,9 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 public class CommunicationThread extends Thread {
+
+  @Autowired
+  private UserRepository repository;
 
   @Value("${socket.timeout:2000}")
   private Integer timeout;
@@ -44,6 +51,19 @@ public class CommunicationThread extends Thread {
       try {
         TransportEnvelope transportEnvelope;
         while ((transportEnvelope = TransportEnvelope.parseDelimitedFrom(socket.getInputStream())) != null){
+          // TODO: 9/19/17 Move deserialization to structures
+          switch (transportEnvelope.getType()) {
+            case LOGIN_REQUEST:
+              LoginRequest request = LoginRequest.parseFrom(transportEnvelope.getData());
+              logger.debug("Login request by {} w/ password {}",request.getUsername(),request.getPassword());
+              User user = repository.findByUsername(request.getUsername());
+              if(user != null) {
+                logger.debug("User {} exists!",user.getUsername());
+                if(user.getPassword().equals(request.getPassword())){
+                  logger.debug("User {} authenticated",user.getUsername());
+                }
+              }
+          }
           logger.debug("message is: {}", transportEnvelope.getTime());
           OutputStream outputStream = socket.getOutputStream();
           TransportEnvelope responseTransport = transportEnvelope.toBuilder()
